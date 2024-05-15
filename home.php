450fe00d4be299ed458797f0e084c7a39b0f3671
Kunal <Kunalkhan370@gmail.com>
2024-05-15 22:46:02 +0530

    Google tag manager updated

--- home.php
+++ home.php
@@ -49,9 +49,20 @@
   <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
   <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5DZVC5ZX');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DZVC5ZX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <!-- menu-area-start -->
 <header id="menu-area">
   <div class="navigation-wrap start-header start-style">
--- partials/header.php
+++ partials/header.php
@@ -58,7 +58,10 @@
     <!-- End Google Tag Manager -->
     </head>
 <body>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DZVC5ZX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <!-- menu-area-start -->
 <header id="menu-area" class="topsec">
     <div class="navigation-wrap start-header start-style">
